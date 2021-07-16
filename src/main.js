370daf1a6efc8d92be1e512978c8ce5e5d52d348
LionelKarlen <lionel.karlen@protonmail.com>
2021-07-16 20:31:49 +0200

    Add basic setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+    router,
+    vuetify,
+    render: function (h) {
+        return h(App);
+    },
+}).$mount('#app');
