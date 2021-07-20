b0b423368dd3b6437a9cd3f537dd6c55bf499c9e
LionelKarlen <lionel.karlen@protonmail.com>
2021-07-20 17:29:26 +0200

    Clean Client view

---
+++ src/components/TimesheetTable.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-data-table
+        :headers="this.headers"
+        :items="this.entries"
+        hide-default-footer
+        :items-per-page="-1"
+    >
+        <template v-slot:[`item.actions`]="{ item }">
+            <v-icon small class="mr-2" @click="document.alert(item)">
+                mdi-pencil
+            </v-icon>
+            <v-icon small> mdi-delete </v-icon>
+        </template>
+    </v-data-table>
+</template>
+
+<script>
+const { ipcRenderer } = require('electron');
+export default {
+    name: 'TimesheetTable',
+    props: ['invoke', 'arg'],
+    data() {
+        return {
+            entries: [],
+            headers: [
+                {
+                    text: 'Date',
+                    sortable: true,
+                    value: 'Date',
+                },
+                {
+                    text: 'Text',
+                    sortable: true,
+                    value: 'Text',
+                },
+                {
+                    text: 'Hours',
+                    sortable: true,
+                    value: 'Hours',
+                },
+                {
+                    text: 'Amount',
+                    sortable: true,
+                    value: 'Amount',
+                },
+                { text: 'Actions', value: 'actions', sortable: false },
+            ],
+        };
+    },
+    async mounted() {
+        ipcRenderer.invoke(this.invoke, this.arg).then(async (data) => {
+            console.log(data);
+            let entries = data;
+            for (let i = 0; i < entries.length; i++) {
+                var e = await ipcRenderer.invoke(
+                    'getUserByID',
+                    entries[i].UserID
+                );
+                entries[i].Amount = entries[i].Hours * e.Amount;
+            }
+            this.entries = entries;
+        });
+    },
+};
+</script>
+
+<style></style>
--- src/views/Client.vue
+++ src/views/Client.vue
@@ -4,70 +4,28 @@
             <h1>{{ client.Name }}</h1>
             <h3>{{ client.Address }}</h3>
         </div>
-        <v-data-table
-            :headers="headers"
-            :items="entries"
-            hide-default-footer
-            :items-per-page="-1"
-        >
-            <template v-slot:[`item.actions`]="{ item }">
-                <v-icon small class="mr-2" @click="document.alert(item)">
-                    mdi-pencil
-                </v-icon>
-                <v-icon small> mdi-delete </v-icon>
-            </template>
-        </v-data-table>
+        <timesheet-table invoke="getTimesByID" :arg="this.$route.params.id" />
         <v-btn depressed color="primary"> Export to File </v-btn>
     </div>
 </template>
 
 <script>
 const { ipcRenderer } = require('electron');
+import TimesheetTable from '../components/TimesheetTable.vue';
 export default {
+    components: { TimesheetTable },
     name: 'Client',
     data() {
         return {
             entries: [],
             client: {},
-            headers: [
-                {
-                    text: 'Date',
-                    sortable: true,
-                    value: 'Date',
-                },
-                {
-                    text: 'Text',
-                    sortable: true,
-                    value: 'Text',
-                },
-                {
-                    text: 'Hours',
-                    sortable: true,
-                    value: 'Hours',
-                },
-                {
-                    text: 'Amount',
-                    sortable: true,
-                    value: 'Amount',
-                },
-                { text: 'Actions', value: 'actions', sortable: false },
-            ],
         };
     },
+    components: {
+        TimesheetTable,
+    },
     async mounted() {
-        var id = this.$route.params.id;
-        ipcRenderer.invoke('getTimesByID', id).then(async (data) => {
-            console.log(data);
-            let entries = data;
-            for (let i = 0; i < entries.length; i++) {
-                var e = await ipcRenderer.invoke(
-                    'getUserByID',
-                    entries[i].UserID
-                );
-                entries[i].Amount = entries[i].Hours * e.Amount;
-            }
-            this.entries = entries;
-        });
+        let id = this.$route.params.id;
         ipcRenderer.invoke('getClientByID', id).then((data) => {
             console.log(data);
             this.client = data;
