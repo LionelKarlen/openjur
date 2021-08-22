f4e0a7984f10ed8d1652207acec7b41a045d0959
LionelKarlen <lionel.karlen@protonmail.com>
2021-08-22 18:17:07 +0200

    Fix enter bug

--- src/components/EditEntryDialog.vue
+++ src/components/EditEntryDialog.vue
@@ -108,7 +108,7 @@
                 <v-btn color="blue darken-1" text @click="close">
                     Cancel
                 </v-btn>
-                <v-btn color="blue darken-1" text type="submit"> Save </v-btn>
+                <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
